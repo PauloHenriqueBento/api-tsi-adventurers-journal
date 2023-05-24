26e1942464127489f307a763834f06f3360f7616
Marcos <king.frontweb@outlook.com>
2023-05-24 14:23:49 -0300

    Fix: Correção de imagens nos modalidades

--- database/seeders/ModalidadeSeeder.php
+++ database/seeders/ModalidadeSeeder.php
@@ -14,6 +14,7 @@ class ModalidadeSeeder extends Seeder
      */
     public function run()
     {
+
         $modalidades = [
             [
                 'nome' => 'Paraquedismo',
@@ -21,12 +22,12 @@ class ModalidadeSeeder extends Seeder
                 'photo_path' => 'paraquedismo.png'
             ],
             [
-                'nome' => 'surf',
+                'nome' => 'Surf',
                 'descricao' => 'Domine as ondas e sinta a energia do mar ao praticar esse esporte emocionante.',
                 'photo_path' => 'surf.png'
             ],
             [
-                'nome' => 'rapel',
+                'nome' => 'Rapel',
                 'descricao' => 'Desça paredões e encare desafios verticais com segurança e adrenalina.',
                 'photo_path' => 'rapel.png'
             ],
@@ -35,7 +36,6 @@ class ModalidadeSeeder extends Seeder
                 'descricao' => 'Voe pelos céus e desfrute da incrível sensação de liberdade enquanto plana no ar.',
                 'photo_path' => 'asaDelta.png'
             ],
-
             [
                 'nome' => 'Rafting',
                 'descricao' => 'Aventure-se em emocionantes descidas de rios em um bote inflável, enfrentando corredeiras e desfrutando da natureza.',
@@ -56,7 +56,6 @@ class ModalidadeSeeder extends Seeder
                 'descricao' => 'Desafie-se a subir montanhas e rochas, superando obstáculos e testando suas habilidades.',
                 'photo_path' => 'escalada.png'
             ],
-
             [
                 'nome' => 'Bungee Jump',
                 'descricao' => 'Pule de uma plataforma com uma corda elástica presa ao corpo e experimente uma sensação única de queda livre.',
@@ -77,7 +76,6 @@ class ModalidadeSeeder extends Seeder
                 'descricao' => 'Deslize pelas montanhas cobertas de neve em uma prancha, desafiando as pistas e sentindo a adrenalina do esporte.',
                 'photo_path' => 'snowboard.png'
             ],
-
             [
                 'nome' => 'Esqui',
                 'descricao' => 'Desça montanhas cobertas de neve com esquis nos pés, aproveitando a velocidade e a emoção dessa modalidade.',
@@ -99,6 +97,92 @@ class ModalidadeSeeder extends Seeder
                 'photo_path' => 'parkour.png'
             ],
         ];
+
+        // $modalidades = [
+        //     [
+        //         'nome' => 'Paraquedismo',
+        //         'descricao' => 'Experimente a emoção da queda livre e a sensação de liberdade enquanto desce suavemente com um paraquedas.',
+        //         'photo_path' => 'paraquedismo.png'
+        //     ],
+        //     [
+        //         'nome' => 'surf',
+        //         'descricao' => 'Domine as ondas e sinta a energia do mar ao praticar esse esporte emocionante.',
+        //         'photo_path' => 'surf.png'
+        //     ],
+        //     [
+        //         'nome' => 'rapel',
+        //         'descricao' => 'Desça paredões e encare desafios verticais com segurança e adrenalina.',
+        //         'photo_path' => 'rapel.png'
+        //     ],
+        //     [
+        //         'nome' => 'Asa-Delta',
+        //         'descricao' => 'Voe pelos céus e desfrute da incrível sensação de liberdade enquanto plana no ar.',
+        //         'photo_path' => 'asaDelta.png'
+        //     ],
+
+        //     [
+        //         'nome' => 'Rafting',
+        //         'descricao' => 'Aventure-se em emocionantes descidas de rios em um bote inflável, enfrentando corredeiras e desfrutando da natureza.',
+        //         'photo_path' => 'rafting.png'
+        //     ],
+        //     [
+        //         'nome' => 'Base jump',
+        //         'descricao' => 'Salte de estruturas altas e experimente a descarga de adrenalina em uma modalidade radical.',
+        //         'photo_path' => 'baseJump.png'
+        //     ],
+        //     [
+        //         'nome' => 'Trekking',
+        //         'descricao' => 'Explore trilhas incríveis e descubra paisagens deslumbrantes em caminhadas pela natureza.',
+        //         'photo_path' => 'trekking.png'
+        //     ],
+        //     [
+        //         'nome' => 'Escalada',
+        //         'descricao' => 'Desafie-se a subir montanhas e rochas, superando obstáculos e testando suas habilidades.',
+        //         'photo_path' => 'escalada.png'
+        //     ],
+
+        //     [
+        //         'nome' => 'Bungee Jump',
+        //         'descricao' => 'Pule de uma plataforma com uma corda elástica presa ao corpo e experimente uma sensação única de queda livre.',
+        //         'photo_path' => 'bungeeJump.png'
+        //     ],
+        //     [
+        //         'nome' => 'Canoagem',
+        //         'descricao' => 'Navegue em rios e lagos, explorando paisagens deslumbrantes e enfrentando desafios na água.',
+        //         'photo_path' => 'canoagem.png'
+        //     ],
+        //     [
+        //         'nome' => 'Mergulho',
+        //         'descricao' => 'Descubra o fascinante mundo subaquático, explorando recifes de corais e encontrando uma diversidade incrível de vida marinha.',
+        //         'photo_path' => 'mergulho.png'
+        //     ],
+        //     [
+        //         'nome' => 'Snowboard',
+        //         'descricao' => 'Deslize pelas montanhas cobertas de neve em uma prancha, desafiando as pistas e sentindo a adrenalina do esporte.',
+        //         'photo_path' => 'snowboard.png'
+        //     ],
+
+        //     [
+        //         'nome' => 'Esqui',
+        //         'descricao' => 'Desça montanhas cobertas de neve com esquis nos pés, aproveitando a velocidade e a emoção dessa modalidade.',
+        //         'photo_path' => 'esqui.png'
+        //     ],
+        //     [
+        //         'nome' => 'Kitesurf',
+        //         'descricao' => 'Combine o poder do vento com a habilidade de manobrar uma prancha na água, criando uma experiência única de esporte aquático.',
+        //         'photo_path' => 'kitesurf.png'
+        //     ],
+        //     [
+        //         'nome' => 'Skateboarding',
+        //         'descricao' => 'Execute manobras radicais em uma prancha com rodas, desafiando a gravidade e mostrando estilo.',
+        //         'photo_path' => 'skateboarding.png'
+        //     ],
+        //     [
+        //         'nome' => 'Parkour',
+        //         'descricao' => 'Supere obstáculos urbanos de forma ágil e criativa, combinando saltos, escaladas e movimentos acrobáticos.',
+        //         'photo_path' => 'parkour.png'
+        //     ],
+        // ];
 
         foreach ($modalidades as $modalidade) {
             // Copiar a imagem para a pasta de armazenamento
